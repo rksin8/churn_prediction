668c9fdbe562220b231a64400dadc601d26f761d
Ranjeet Singh <ranjeet.singh2@monash.edu>
2024-10-02 07:38:54 +1000

    implement first draft model

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
+import pandas as pd
+import sys
+
+from models.preprocessing import preprocess_user_input
 
 app = Flask(__name__)
 
-# Load the saved model
-# model = pickle.load(open('best_rf_model.pkl', 'rb'))
+# Load the model objects
+with open('models/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
 
 @app.route('/')
 def home():
@@ -13,21 +19,37 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Get user inputs from form
+    #data = [float(x) for x in request.form.values()]
+    #final_input = np.array([data])
+
     # Get form data
-    data = [float(x) for x in request.form.values()]
-    final_input = np.array([data])
+    user_input = request.form.to_dict()
     
+    # Convert form data to DataFrame
+    data = pd.DataFrame([user_input])
+
+    print(data.info(), file=sys.stdout)
+
+    # save input to csv to test pre-processing steps - one time precesss comment it later
+    # data.to_csv("input.csv", index=False)
+
+    # Preprocess user input
+    processed_input = preprocess_user_input(data)
+
+    ## Make prediction using the preprocessed input
     # Make a prediction
-    #prediction = model.predict(final_input)
-    prediction =[1]
+    prediction = model.predict(processed_input.values)
+
+    print(prediction[0], file=sys.stdout)
     
     # Get the model accuracy
     #accuracy = accuracy_rf * 100  # assuming accuracy_rf is the model's accuracy
-    accuracy = 88.0
+    accuracy = 81.48
     
     # Display the result
     return render_template('index.html', prediction_text=f'Churn Prediction: {"Yes" if prediction[0] == 1 else "No"}',
-                           accuracy_text=f'Random Forest Model Accuracy: {accuracy:.2f}%')
+                           accuracy_text=f'Model Accuracy: {accuracy:.2f}%')
 
 if __name__ == '__main__':
     app.run(debug=True)
